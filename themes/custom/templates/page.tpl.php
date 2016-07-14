2c03c9bfce61a4e9465ed26bfe1e7bf56f958f86
Selena <selena@Selenas-iMac.local>
2016-07-14 15:05:14 +1200

    Custom theme and Custom form module examples

--- themes/custom/templates/page.tpl.php
+++ themes/custom/templates/page.tpl.php
@@ -1,15 +1,3 @@
-<div class="black-header">
-	<div class="custom-user-menu">
-		<?php if ($logged_in): ?>
-			<?php print render($page['user_menu']); ?>
-		<?php else: ?>
-			<ul>
-				<li class="menu"><a href="<?php base_path(); ?>user/login">Log In</a></li>
-			</ul>
-		<?php endif; ?>
-	</div>
-</div>
-
 <div class="custom-header clearfix">
 	<div class="logo-div">
 		<?php if ($logo): ?>
@@ -23,28 +11,43 @@
 	</div>
 </div>
 
-<div class="custom-content-container-div clearfix">
-	<?php if ($messages): ?>
-		<div id="messages">
-			<div class="section clearfix">
-				<?php print $messages; ?>
-			</div>
+<div class="main-content-area">
+	<div class="main-content">
+		<div class="custom-content-container-div clearfix">
+			<?php if ($messages): ?>
+				<div id="messages">
+					<div class="section clearfix">
+						<?php print $messages; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+			 
+			<?php if ($breadcrumb): ?>
+				<div id="breadcrumb"><?php print $breadcrumb; ?></div>
+			<?php endif; ?>
+			 
+			<h1><?php print $title; ?></h1>
+			 
+			<?php print render($page['content']); ?>
 		</div>
-	<?php endif; ?>
-	 
-	<?php if ($breadcrumb): ?>
-		<div id="breadcrumb"><?php print $breadcrumb; ?></div>
-	<?php endif; ?>
-	 
-	<h1><?php print $title; ?></h1>
-	 
-	<?php print render($page['content']); ?>
-</div>
-	
+		<div class="custom-sidebar-right clearfix">
+			<?php if ($page['sidebar-right']): ?>
+			  <div id="sidebar-right" class="column sidebar"><div class="section">
+			    <?php print render($page['sidebar-right']); ?>
+			  </div></div> <!-- /.section, /#sidebar-right -->
+			<?php endif; ?>	
+		</div>
+	</div>
+</div>	
 
-<div class="custom-footer">
+<div class="footer">
 	<hr />
-	<div class="custom-footer-text">
-		&copy; 2013 custom. All rights reserved.
+    <?php if ($page['footer']): ?>
+    <div id="footer" class="clearfix">
+      <?php print render($page['footer']); ?>
+    </div> <!-- /#footer -->
+  <?php endif; ?>
+	<div class="footer-text">
+		&copy; 2016 custom. All rights reserved.
 	</div>
 </div>
